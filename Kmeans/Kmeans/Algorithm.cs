aa5c30ac178c8c793d927568e6a38bfd4d35ffea
im1sha <vkporsche@ya.ru>
2019-02-06 15:52:27 +0300

    struct Point added, minor changes in the class of Algorithm

--- Kmeans/Kmeans/Algorithm.cs
+++ Kmeans/Kmeans/Algorithm.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 
 namespace Kmeans
 {
@@ -11,50 +10,75 @@ namespace Kmeans
     {
         private readonly int MinPoints = 1_000;
         private readonly int MaxPoints = 100_000;
-        private int TotalPoints;
+
+        private int TotalPointsValue;      
+        public int TotalPoints
+        {
+            get { return TotalPointsValue; }
+            private set
+            {
+                if (InBounds(value, MinPoints, MaxPoints))
+                {
+                    TotalPointsValue = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("totalPoints");
+                }
+            }
+        }
 
         private readonly int MinClusters = 2;
         private readonly int MaxClusters = 20;
-        private int TotalClusters ;
+
+        private int TotalClustersValue; 
+        public int TotalClusters {
+            get { return TotalClustersValue; }
+            private set
+            {
+                if (InBounds(value, MinClusters, MaxClusters))
+                {
+                    TotalClustersValue = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("totalClusters");
+                }
+            }
+        }
 
         private Point[] PositionsOfPoints;
 
-        private int MaxValueOfX;
-        private int MaxValueOfY;
+        private readonly int LowerMaxValueOfX = 100;
+        private readonly int LowerMaxValueOfY = 100;
 
-        private int IterationsDone = 0;
+        public int UpperBoundOfX { get; private set; }
+        public int UpperBoundOfY { get; private set; }
 
-        private bool InBounds(int target, int lowerBound, int UpperBound)
+        public int IterationsDone { get; private set; } = 0;
+
+        /// <summary>
+        /// Detects whether passed number is possible
+        /// </summary>
+        /// <param name="target">Value to check</param>
+        /// <param name="lowerBound">Minimum value</param>
+        /// <param name="upperBound">Maximum value</param>
+        /// <returns>true if target value in passed bounds</returns>
+        private bool InBounds(int target, int lowerBound, int upperBound)
         {
-            return (target >= lowerBound) && (target <= UpperBound);
+            return (target >= lowerBound) && (target <= upperBound);
         }
 
-        public Algorithm(int totalPoints, int totalClusters): this()
+        public Algorithm(int totalPoints, int totalClusters, int upperXBound, int upperYBound)
+            : this()
         {
-            if (InBounds(totalPoints, MinPoints, MaxPoints))
-            {
-                TotalPoints = totalPoints;
-            }
-            else
-            {
-                throw new ArgumentOutOfRangeException("totalPoints");
-            }
-
-            if (InBounds(totalClusters, MinClusters, MaxClusters))
-            {
-                TotalClusters = totalClusters;
-            }
-            else
-            {
-                throw new ArgumentOutOfRangeException("totalClusters");
-            }
+            TotalPoints = totalPoints;
+            TotalClusters = totalClusters;
 
         }
 
         private Algorithm()
         {
-            MaxValueOfX = 500;
-            MaxValueOfY = 500;
         }
 
         public Point[] CreatePointPositions()
@@ -65,8 +89,8 @@ namespace Kmeans
 
             for (int i = 0; i < TotalPoints; i++)
             {
-                PositionsOfPoints[i] = new Point(random.Next() % MaxValueOfX,
-                    random.Next() % MaxValueOfY);
+                PositionsOfPoints[i] = new Point(random.Next() % UpperBoundOfX,
+                    random.Next() % UpperBoundOfY);
             }
 
             return (Point[]) PositionsOfPoints.Clone();
---
+++ Kmeans/Kmeans/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kmeans
+{
+    struct Point 
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Cluster = -1;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public int Cluster { get; set; } 
+    }
+}
